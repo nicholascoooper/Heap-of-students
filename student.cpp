9b4567d82d5706de55d1c8d988bd7382742dba37
nicholascoooper <nicholascoopernsc@gmail.com>
2025-10-04 03:56:27 +0000

    time

---
+++ student.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include "student.h"
+
+Student::Student(){
+	Student::fullStudent = " ";
+	Student::firstName = " ";
+	Student::lastName = " ";
+	Student::address = " ";
+	Student::birthdate = " ";
+	Student::gradDate = " ";
+	Student::credits = 0;
+}
+
+string Student::getFirstName(){
+	Student::firstName = firstName;
+}
+
+string Student::getLastName(){
+	Student::lastName = lastName;
+}
+
+void Student::init(fullStudent){
+	Student::fullStudent = fullStudent;
+}
+
+void Student::printStudent(){
+	std::cout<<fullStudent;
+}
+
+
+
+
